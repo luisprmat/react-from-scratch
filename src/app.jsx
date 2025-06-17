c56e881e1f6e622ef6d35022df6d3d10c1d4a0fd
Luis Parrado <luisprmat@gmail.com>
2025-06-16 20:43:51 -0500

    L08-Quick housekeeping

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,3 @@
-import { createRoot } from 'react-dom/client'
-import './app.css'
-
-function App() {
+export function App() {
   return <h1 className='font-mono text-4xl'>React & Vite</h1>
 }
-
-const app = document.getElementById('app')
-const root = createRoot(app)
-
-root.render(<App />)
